e3f6b7455f7e0ed0dbef13d6bd476f4927ae2e84
Richard Wang <richardwa@google.com>
2020-01-07 13:06:33 +0900

    Fix GuavaJS wrapper compilation

--- css/guavajs-wrapper.js
+++ css/guavajs-wrapper.js
@@ -1,9 +1,9 @@
 let fs = require('fs');
 
 // https://stackoverflow.com/a/5809968
-eval(fs.readFileSync('./node_modules/guava-js/src/main/javascript/GuavaJS.js')+'');
-eval(fs.readFileSync('./node_modules/guava-js/src/main/javascript/GuavaJS.strings.js')+'');
-eval(fs.readFileSync('./node_modules/guava-js/src/main/javascript/GuavaJS.strings.charmatcher.js')+'');
-eval(fs.readFileSync('./node_modules/guava-js/src/main/javascript/GuavaJS.strings.splitter.js')+'');
+eval(fs.readFileSync(require.resolve('guava-js/src/main/javascript/GuavaJS.js'))+'');
+eval(fs.readFileSync(require.resolve('guava-js/src/main/javascript/GuavaJS.strings.js'))+'');
+eval(fs.readFileSync(require.resolve('guava-js/src/main/javascript/GuavaJS.strings.charmatcher.js'))+'');
+eval(fs.readFileSync(require.resolve('guava-js/src/main/javascript/GuavaJS.strings.splitter.js'))+'');
 
 module.exports = GuavaJS;
